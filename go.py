2a451d680b55bf908aba5706b7a726ec8e31f368
stl-23 <73641317+stl-23@users.noreply.github.com>
2021-05-19 17:07:12 +0800

    Add files via upload

--- go.py
+++ go.py
@@ -1,39 +1,59 @@
 import os
-import sys
 
-def makeGO(file,group1,group2,out_dir,prefix,OrgDb='org.Hs.eg.db',):
+def makeGO(file,out_dir,prefix,OrgDb='org.Hs.eg.db',):
     ## code from https://blog.csdn.net/sinat_30623997/article/details/79250940?utm_source=blogxgwz1
     out = os.path.join(out_dir, prefix + "_GO_out.tsv")
     cmd = """library(clusterProfiler)
+library(ggplot2)
 data <- read.table(file="%(file)s",header=T,sep = "\t")
 target_gene_id <- unique(data$GeneID)
 ## out file
 ego_ALL <- enrichGO(OrgDb="%(OrgDb)s",
                    gene = target_gene_id,
+                   pvalueCutoff = 0.05,
+                   pAdjustMethod = "BH",
                    qvalueCutoff = 0.05,
                    ont = "ALL",
                    readable=TRUE)
-write.tsv(data.frame(ego_ALL@result),'%(out)s',quote=FALSE)
+write.tsv(data.frame(ego_ALL@result),'%(out)s',quot=FALSE)
 ## draw
 display_num <- c(15,10,15)
 ego_MF <- enrichGO(OrgDb="%(OrgDb)s",
              gene = target_gene_id,
+             pvalueCutoff = 0.05,
+             pAdjustMethod = "BH",
              qvalueCutoff = 0.05,
              ont = "MF",
              readable=TRUE)
-ego_result_MF <- as.data.frame(ego_MF)[1:display_num[1],]
+MF_table <- as.data.frame(ego_MF)
+if (nrow(MF_table) < display_num[1]){
+    ego_result_MF <- MF_table
+    }else{
+    ego_result_MF <- MF_table[1:display_num[1],]
 ego_CC <- enrichGO(OrgDb="%(OrgDb)s",
                    gene = target_gene_id,
+                   pvalueCutoff = 0.05,
+                   pAdjustMethod = "BH",
                    qvalueCutoff = 0.05,
                    ont = "CC",
                    readable=TRUE)
-ego_result_CC <- as.data.frame(ego_CC)[1:display_num[2],]
+CC_table <- as.data.frame(ego_CC)
+if (nrow(CC_table) < display_num[2]){
+    ego_result_CC <- CC_table
+    }else{
+    ego_result_CC <- CC_table[1:display_num[2],]
 ego_BP <- enrichGO(OrgDb="%(OrgDb)s",
                    gene = target_gene_id,
+                   pvalueCutoff = 0.05,
+                   pAdjustMethod = "BH",
                    qvalueCutoff = 0.05,
                    ont = "BP",
                    readable=TRUE)
-ego_result_BP <- as.data.frame(ego_BP)[1:display_num[3],]
+BP_table <- as.data.frame(ego_BP)
+if (nrow(BP_table) < display_num[3]){
+    ego_result_BP <- BP_table
+    }else{
+    ego_result_BP <- BP_table[1:display_num[3],]
 
 go_enrich_df <- data.frame(ID=c(ego_result_BP$ID, ego_result_CC$ID, ego_result_MF$ID),
 Description=c(ego_result_BP$Description, ego_result_CC$Description, ego_result_MF$Description),
@@ -65,7 +85,7 @@ names(labels) = rev(1:nrow(go_enrich_df))
 
 ## colors for bar // green, blue, orange
 CPCOLS <- c("#8DA1CB", "#FD8D62", "#66C3A5")
-library(ggplot2)
+
 p <- ggplot(data=go_enrich_df, aes(x=number, y=GeneNumber, fill=type)) +
   geom_bar(stat="identity", width=0.8) + coord_flip() +
   scale_fill_manual(values = CPCOLS) + theme_bw() +
@@ -73,7 +93,6 @@ p <- ggplot(data=go_enrich_df, aes(x=number, y=GeneNumber, fill=type)) +
   xlab("GO term") +
   theme(axis.text=element_text(face = "bold", color="gray50")) +
   labs(title = "The Most Enriched GO Terms")
-
 p
 
 pdf("%(out_dir)s/%(prefix)s_go_enrichment.pdf")
@@ -83,6 +102,20 @@ dev.off()
 svg("%(out_dir)s/%(prefix)s_go_enrichment.svg")
 p
 dev.off()
+
+p2 <- plotGOgraph(ego_MF)
+pdf("%(out_dir)s/%(prefix)s_go_MF_DAG.pdf")
+p2
+dev.off()
+p3 <- plotGOgraph(ego_CC)
+pdf("%(out_dir)s/%(prefix)s_go_CC_DAG.pdf")
+p3
+dev.off()
+p4 <- plotGOgraph(ego_BP)
+pdf("%(out_dir)s/%(prefix)s_go_BP_DAG.pdf")
+p4
+dev.off()
+
 """ %{'file':file,'OrgDb':OrgDb,'out':out,'out_dir':out_dir,'prefix':prefix}
 
     return cmd
