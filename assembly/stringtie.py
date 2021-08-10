61da1a10be3dbd63d14607e1036496e3faa970e6
stl <73641317+stl-23@users.noreply.github.com>
2021-08-10 15:58:05 +0800

    Add files via upload

--- assembly/stringtie.py
+++ assembly/stringtie.py
@@ -1,19 +1,17 @@
 import os
-import sys
-import rnaseq.getmyconfig as getmyconfig
-import rnaseq.utils as utils
+from utils import getConfig,makedir
 
-stringtie = getmyconfig('Transcript','stringtie').strip("'")
+stringtie = getConfig('Transcript','stringtie')
 
-def assemblyandquantitation(samples,bams,gtf,out_dir,threads=8,ref_only=False):
+def assemblyandquantitation(samples,bams,gtf,out_dir,threads=8,merge=True):
     dic = {}
     gtfs = []
     for index,sample in enumerate(samples):
         bam = bams[index]
         out = os.path.join(out_dir, sample)
-        out_name = os.path.join(out,sample)
-        utils(out)
-        if not ref_only: ## reference and assembled transcripts
+        out_name = os.path.join(out, sample)
+        makedir(out)
+        if merge: ## reference and assembled transcripts
             ### transcripts prediction for each sample
             cmd_run = f"{stringtie} {bam} -p {threads} -G {gtf} -l {sample} -o {out_name}.gtf".format(**locals())
             gtfs.append("{out}.gtf".format(out=out_name))
@@ -21,14 +19,13 @@ def assemblyandquantitation(samples,bams,gtf,out_dir,threads=8,ref_only=False):
             cmd_count = f"{stringtie} {bam} -e -B -p {threads} -G {out_dir}/merged.gtf -o {out_name}.out.gtf " \
                     f"-A {out_name}.abundance.tab ".format(**locals())
             dic[sample] = [cmd_run,cmd_count]
-        else:             ## reference only
-            cmd_count = f"{stringtie} {bam} -e -p {threads} -G {gtf} -l {sample} -o {out_name}.out.gtf " \
+        else:     ## reference only
+            cmd_count = f"{stringtie} {bam} -e -B -p {threads} -G {gtf} -l {sample} -o {out_name}.out.gtf " \
                         f"-A {out_name}.abundance.tab".format(**locals())
             dic[sample] = [cmd_count]
-    if not ref_only:
+    if merge:
         ### merge reference and assembly gtf files
         gtf_files = ' '.join(gtfs)
         cmd_merge = f"{stringtie} --merge -p {threads} -G {gtf} -o {out_dir}/merged.gtf {gtf_files}".format(**locals())
         dic['merge'] = cmd_merge
-
     return dic
