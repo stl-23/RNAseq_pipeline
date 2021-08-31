15800f6e28239ec4afb248b689270d8b44704c71
stl <73641317+stl-23@users.noreply.github.com>
2021-08-31 15:47:17 +0800

    Add files via upload

--- enrich/go.R
+++ enrich/go.R
@@ -14,8 +14,18 @@ db <-  args[4]
 out_file <- paste0(out_path,'/',prefix,"_GO_out.tsv")
 data <- read.table(file=input_file,header=T,sep = "\t")
 target_gene_id <- unique(data$GeneID)
-target_gene_id_changed <- bitr(target_gene_id, fromType="SYMBOL", toType="ENTREZID", OrgDb=db)
-target_gene_id_changed <- target_gene_id_changed$ENTREZID
+if (grepl('^ENSG',target_gene_id[1])){
+    target_gene_id_changed <- bitr(target_gene_id, fromType="ENSEMBL", toType="ENTREZID", OrgDb=db)
+    target_gene_id_changed <- target_gene_id_changed$ENTREZID
+    }else if (grepl('^NG_',target_gene_id[1])| grepl('^NM_',target_gene_id[1]) |grepl('^NP_',target_gene_id[1]) ){
+    target_gene_id_changed <- bitr(target_gene_id, fromType="REFSEQ", toType="ENTREZID", OrgDb=db)
+    target_gene_id_changed <- target_gene_id_changed$ENTREZID
+    }else if (grepl('^[0-9]+',target_gene_id[1])){
+    target_gene_id_changed <- target_gene_id
+    }else {
+    target_gene_id_changed <- bitr(target_gene_id, fromType="SYMBOL", toType="ENTREZID", OrgDb=db)
+    target_gene_id_changed <- target_gene_id_changed$ENTREZID
+    }
 ## out file
 ego_ALL <- enrichGO(OrgDb=db,
                    gene = target_gene_id_changed,
--- enrich/kegg.R
+++ enrich/kegg.R
@@ -12,8 +12,22 @@ Org <-  args[4]
 outfile <- paste0(out_path,'/',prefix,'_KEGG_out.tsv')
 data <- read.table(file=input_file,header=T,sep = "\t")
 target_gene_id <- unique(data$GeneID)
-target_gene_id_changed <- bitr(target_gene_id, fromType="SYMBOL", toType="ENTREZID", OrgDb="org.Hs.eg.db") ## human ID
-target_gene_id_changed <- target_gene_id_changed$ENTREZID
+if (Org == 'hsa'){
+    db <- 'org.Hs.eg.db'
+    }
+if (grepl('^ENSG',target_gene_id[1])){
+    target_gene_id_changed <- bitr(target_gene_id, fromType="ENSEMBL", toType="ENTREZID", OrgDb=db)
+    target_gene_id_changed <- target_gene_id_changed$ENTREZID
+    }else if (grepl('^NG_',target_gene_id[1])| grepl('^NM_',target_gene_id[1]) |grepl('^NP_',target_gene_id[1]) ){
+    target_gene_id_changed <- bitr(target_gene_id, fromType="REFSEQ", toType="ENTREZID", OrgDb=db)
+    target_gene_id_changed <- target_gene_id_changed$ENTREZID
+    }else if (grepl('^[0-9]+',target_gene_id[1])){
+    target_gene_id_changed <- target_gene_id
+    }else {
+    target_gene_id_changed <- bitr(target_gene_id, fromType="SYMBOL", toType="ENTREZID", OrgDb=db)
+    target_gene_id_changed <- target_gene_id_changed$ENTREZID
+    }
+
 kegg_df <- enrichKEGG(target_gene_id_changed,
                       organism=Org,
                       pvalueCutoff = 0.05,
