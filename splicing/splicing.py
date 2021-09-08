7b04f591fa532f4cffcb2a202da6c26b6ddbdffa
stl <73641317+stl-23@users.noreply.github.com>
2021-09-08 09:54:51 +0800

    Update splicing.py

--- splicing/splicing.py
+++ splicing/splicing.py
@@ -8,7 +8,7 @@ def alternative_splicing(path,gtf,paired_or_single="paired",readlength=50,theads
     tmp = os.path.join(path,'temp')
     b1 = os.path.join(tmp,'b1.txt')
     b2 = os.path.join(tmp,'b2.txt')
-    cmd = f"{python3} {rmats} --b1 {b1} --b2 {b2} -t {paired_or_single} --gtf {gtf} --readLength {readlength} --variable-read-length \
+    cmd = f"source ~/.bashrc && {python3} {rmats} --b1 {b1} --b2 {b2} -t {paired_or_single} --gtf {gtf} --readLength {readlength} --variable-read-length \
     --od {path} --tmp {tmp} --nthread {theads}".format(**locals())
 
     return cmd
